63416c647afc50a7e37a03cce3757b880d2a24f6
Rajdeep Singh <radewala.2@gmail.com>
2024-10-03 21:52:44 +0530

    update UI

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -36,11 +36,11 @@ export default async function page({ params }: { params: { slug: string } }) {
   return (
     <>
       <div className="mx-auto w-full max-w-7xl px-5 py-5 md:px-10 md:py-8">
-        <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/blog" >
+        <Link className="inline-flex items-center gap-2 px-4 py-1.5 rounded bg-[#050708] text-white hover:underline hover:underline-offset-4" href="/blog" >
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m15 18-6-6 6-6" /></svg>
           Back
         </Link>
-        <div className="mb-6 bg-gray-50 p-5">
+        <div className="mb-6 bg-gray-50 p-5 mt-5">
           <Link href={`tag/${post.tags[0]}`} className="capitalize mb-2 font-bold px-2 py-3 inline-block"> {post.tags[0]} </Link>
           <h2 className="mb-6 text-3xl font-bold md:text-5xl lg:mb-10"> {post.title} </h2>
           <p className="mb-8 text-sm text-gray-500 sm:text-base lg:mb-24"> {post.body} </p>
