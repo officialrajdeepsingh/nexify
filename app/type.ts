a05b74d0520fd32c6aa2c835c21d6014d1ec69e3
Rajdeep Singh <radewala.2@gmail.com>
2024-10-06 21:02:10 +0530

    modify comment

--- app/type.ts
+++ app/type.ts
@@ -14,3 +14,22 @@ export interface TypePost {
   skip: number
   limit: number
 }
+
+export interface TypeComment {
+  id: number;
+  body: string,
+  postId: number,
+  likes: number,
+  user: {
+    id: number,
+    username: string,
+    fullName: string
+  }
+}
+
+export interface TypeComments {
+  comments: TypeComment[];
+  skip: number;
+  total: number;
+  limit: number
+}
