b6525a32bf91df90e25c6e2535d9a41bd9f9a3ce
Rajdeep Singh <radewala.2@gmail.com>
2024-10-06 21:02:35 +0530

    robots.txt file

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: 'http://localhost:3000/sitemap.xml',
+    host: "http://localhost:3000/"
+  }
+}
