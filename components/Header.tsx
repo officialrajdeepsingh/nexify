02e7d81746c524df4b457d81957af2981a527761
Rajdeep Singh <radewala.2@gmail.com>
2024-11-27 15:26:05 +0530

    update the header ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export function Header() {
   const segment = useSelectedLayoutSegment();
 
   return (
-    <header className="row-start-3 flex gap-6 pt-6 pb-4 flex-wrap items-center justify-center">
+    <header role="header-main" className="row-start-3 flex gap-6 pt-6 pb-4 flex-wrap items-center justify-center">
       <Link className={cn("flex items-center gap-2 hover:underline hover:underline-offset-4", segment === null && "underline underline-offset-4")} href="/">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M3 11.8284C3 11.2979 3.21071 10.7892 3.58579 10.4142L10.5858 3.41416C11.3668 2.63311 12.6332 2.63311 13.4142 3.41416L20.4142 10.4142C20.7893 10.7892 21 11.2979 21 11.8284V20C21 20.5522 20.5523 21 20 21H14C13.4477 21 13 20.5522 13 20V15H11V20C11 20.5522 10.5523 21 10 21H4C3.44772 21 3 20.5522 3 20V11.8284ZM12 4.82837L5 11.8284V19H9V13.9999C9 13.4477 9.44772 12.9999 10 12.9999H14C14.5523 12.9999 15 13.4477 15 13.9999V19H19V11.8284L12 4.82837Z" fill="black" />
@@ -35,5 +35,3 @@ export function Header() {
     </header>
   );
 }
-
-//  <Link className={segment === "blog" ? "flex items-center gap-2 bg-blue-50 underline underline-offset-4" : "flex items-center gap-2 hover:underline hover:underline-offset-4"} href="/blog">
