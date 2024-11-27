cfdb141b639acb351e3cc5cec8c983fd17f20bd7
Rajdeep Singh <radewala.2@gmail.com>
2024-11-27 15:23:10 +0530

    update the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
 export default async function page() {
   return (
     <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-20">
-      <div className="mb-6 bg-gray-50 p-5">
+      <div className="mb-6 bg-blue-400 p-5">
         <h2 className="mb-6 text-3xl font-bold md:text-5xl lg:mb-10"> About us </h2>
-        <p className="mb-8 text-sm text-gray-500 sm:text-base lg:mb-24"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pellentesque urna et ipsum ultricies, id dignissim justo tempor. Proin cursus dapibus porttitor. </p>
+        <p className="mb-8 text-sm sm:text-base lg:mb-24"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce pellentesque urna et ipsum ultricies, id dignissim justo tempor. Proin cursus dapibus porttitor. </p>
       </div>
 
     </div>
