2a9094549fa34609f50755ad4d5ace024bb58e9c
Rajdeep Singh <radewala.2@gmail.com>
2024-11-27 15:26:18 +0530

    update the tags ui

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -4,18 +4,14 @@ import Link from 'next/link'
 import { useSelectedLayoutSegment } from 'next/navigation';
 import { cn } from '@/lib/utils';
 
-
 const tags: string[] = ["history", "american", "crime", "french", "fiction"]
 
 export default function Tags() {
-
   const segment = useSelectedLayoutSegment();
-
   return (
     <div className="my-10 md:my-20 flex flex-col md:flex-row justify-center gap-3">
       {
-        tags.map(tag => <Link key={tag} href={`/blog/tag/${tag}`} className={cn(
-          "px-4 py-2 bg-gray-100 font-semibold rounded-full capitalize", segment === tag && "bg-black text-white")}> {tag} </Link>
+        tags.map(tag => <Link key={tag} href={`/blog/tag/${tag}`} className={cn("px-4 py-2 bg-gray-100 font-semibold rounded-full capitalize", segment === tag && "bg-black text-white")}> {tag} </Link>
         )
       }
     </div>
