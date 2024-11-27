6199de281ce9a5bf5116344d2bfd3590a4c2b3c3
Rajdeep Singh <radewala.2@gmail.com>
2024-11-27 15:23:41 +0530

    update the contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,6 @@ import Script from 'next/script';
 export const metadata: Metadata = {
   title: 'Contact us',
   description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit ut.',
-  
   openGraph: {
     title: 'Contact us',
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit ut.',
@@ -16,7 +15,6 @@ export const metadata: Metadata = {
       height: 719
     }]
   },
- 
   twitter: {
     site: "https://medium.com/@officialrajdeepsingh",
     creator: "Official_R_deep",
@@ -35,16 +33,13 @@ export const metadata: Metadata = {
 export default function page() {
 
   return (
-    <section className="bg-blue-100">
-      <div className="mx-auto max-w-7xl px-5 py-16 text-center md:px-10 md:py-20">
-        <h2 className="text-3xl font-bold md:text-5xl">Contact us</h2>
-        <p className="mx-auto mb-8 mt-4 max-w-lg text-gray-500 md:mb-12 lg:mb-16">
-          Lorem ipsum dolor sit amet consectetur adipiscing elit ut
-          aliquam,purus sit amet luctus magna fringilla urna
-        </p>
-        <div data-mf-id="xkI6W32" data-mf-title="Untitled Form" className='w-full h-[750px]' ></div>
-        <Script src="https://minform.io/embed.js"></Script>
-      </div>
+    <section className="bg-blue-400 mx-auto max-w-7xl px-5 py-16 text-center md:px-10 md:py-20">
+      <h2 className="text-3xl font-bold md:text-5xl">Contact us</h2>
+      <p className="mx-auto mb-8 mt-4 max-w-lg text-gray-800 md:mb-12 lg:mb-16">
+        Lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam,purus sit amet luctus magna fringilla urna
+      </p>
+      <div data-mf-id="xkI6W32" data-mf-title="Untitled Form" className='w-full h-[750px]' ></div>
+      <Script src="https://minform.io/embed.js"></Script>
     </section>
   );
 
